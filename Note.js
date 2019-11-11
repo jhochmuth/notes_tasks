944a28f835af51f35cdbd1bb6c738dba53a69a9b
Julius <jhochmuth@comcast.net>
2019-11-11 09:48:54 -0700

    Notes can be dragged.

--- Note.js
+++ Note.js
@@ -1,11 +1,10 @@
 const React = require('react');
+const clickdrag = require('react-clickdrag');
 
 let mainStyle = {
   borderStyle: "solid",
   height: "300px",
   width: "300px",
-  left: "150px",
-  top: "150px",
   position: "relative"
 }
 
@@ -20,10 +19,41 @@ let textStyle = {
 class Note extends React.Component {
   constructor(props) {
     super();
-    this.state = {attrs: props.noteData.attrs}
+    this.state = {
+      attrs: props.noteData.attrs,
+      lastPositionX: 0,
+      lastPositionY: 0,
+      currentX: 0,
+      currentY: 0
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.dataDrag.isMoving) {
+      this.setState({
+        currentX: this.state.lastPositionX + nextProps.dataDrag.moveDeltaX,
+        currentY: this.state.lastPositionY + nextProps.dataDrag.moveDeltaY
+      });
+    }
+    else {
+      this.setState({
+        lastPositionX: this.state.currentX,
+        lastPositionY: this.state.currentY
+      })
+    }
   }
 
   render() {
+    let translation = 'translate('+this.state.currentX+'px, '+this.state.currentY+'px)';
+
+    let mainStyle = {
+      borderStyle: "solid",
+      height: "300px",
+      width: "300px",
+      position: "relative",
+      transform: [translation]
+    }
+
     return (
       <div style={mainStyle}>
         <h3 style={titleStyle}>{this.state.attrs.title}</h3>
@@ -37,4 +67,6 @@ class Note extends React.Component {
 
 }
 
-module.exports = Note;
+var NoteClickDrag = clickdrag.default(Note, {touch: true});
+
+module.exports = NoteClickDrag;
