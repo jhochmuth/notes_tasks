d428e970bc1a34f49678e097865ef59a10e66592
Julius <jhochmuth@comcast.net>
2020-04-16 11:52:44 -0600

    Changed grpc files.

--- server.py
+++ server.py
@@ -73,7 +73,8 @@ class NoteServicer(tasks_pb2_grpc.NoteManagerServicer):
         return tasks_pb2.NoteReply(id=descendant.id,
                                    attrs=descendant.attrs,
                                    parent_container_id=container_id,
-                                   prototype_id=note.id)
+                                   prototype_id=note.id,
+                                   inherited_attrs=list(descendant.inherited_attrs))
 
     def DeleteNoteAttr(self, request, context):
         note = documents[request.document_id].children[request.note_id]
